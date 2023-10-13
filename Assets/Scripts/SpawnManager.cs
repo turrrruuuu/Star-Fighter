b078b0c29974beeba7a11590a0a6ba728eb616ec
turrrruuuu <turuumendbayr@gmail.com>
2023-10-13 00:30:25 -0700

    Added Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
     private GameObject _enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
     }
-    //IEnumerator SpawnRoutine()
-    //{
-    //    while (true)
-    //    {
-    //        float randX = Random.Range(-9.2f, 9.2f);
-    //        Instantiate(_enemy, transform.position(randX, 6, 0),
-    //    }
-    //}
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            float randX = Random.Range(-9.2f, 9.2f);
+            Vector3 position = new Vector3(randX, 6, 0);
+            Instantiate(_enemy, position,Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
 }
